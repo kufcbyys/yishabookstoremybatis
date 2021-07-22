0c0b12a77d2aa9454318f97edc8ae48627f427a8
kufcbyys <86660369+kufcbyys@users.noreply.github.com>
2021-07-22 19:25:22 +0800

    Update BookMapper.java

--- src/main/java/com/example/yishabookstoremybatis/mapper/BookMapper.java
+++ src/main/java/com/example/yishabookstoremybatis/mapper/BookMapper.java
@@ -6,28 +6,9 @@ import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
-/**
- * 如上图代码：使用@Mapper注解要定义成一个接口interface
- * 作用：
- * 1.使用@Mapper将NewsDAO接口交给Spring进行管理
- * 2.不用写Mapper映射文件（XML）
- * 3.为这个NewsDAO接口生成一个实现类，让别的类进行引用
- */
 @Mapper
 public interface BookMapper {
-    /**
-     *
-     @Select、@Insert、@Update 以及 @Delete 四个注解分别对应 XML 中的 select、insert、
-        update 以及 delete 标签，@Results 注解类似于 XML 中的 ResultMap 映射文件（getUserById
-        方法给查询结果的字段取别名主要是向小伙伴们演示下 @Results 注解的用法）。
-     另外使用 @SelectKey 注解可以实现主键回填的功能，即当数据插入成功后，插入成功的数据 id会赋值到 user 对象的id 属性上。
-     UserMapper2 创建好之后，还要配置 mapper 扫描，有两种方式，一种是直接在 UserMapper2 上面添加 @Mapper 注解，
-     这种方式有一个弊端就是所有的 Mapper 都要手动添加，要是落下一个就会报错，
-        还有一个一劳永逸的办法就是直接在启动类上添加 Mapper 扫描@MapperScan(basePackages = "xxx.mapper")
-      * @return
-     */
-//    @Select("select * from book")
-//    List<Book> getAllBooks();
+
 
     //是否存在用户
     @Select("SELECT COUNT(username) FROM USER")
